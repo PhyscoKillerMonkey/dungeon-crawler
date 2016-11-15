f1f74ded84ab4b0d59e7cc1c23e2cb6648b48608
Reece Bennett <reece.bennett1@gmail.com>
2016-11-15 22:35:02 +0000

    Setup player animations

--- dist/game.js
+++ dist/game.js
@@ -20,8 +20,14 @@ var game = new ex.Engine({
     width: grid.size * grid.width * grid.scale,
     height: grid.size * grid.height * grid.scale
 });
-var txPlayer = new ex.Texture("/assets/player.png");
-var loader = new ex.Loader([txPlayer]);
+// Create the loader and load textures
+var loader = new ex.Loader();
+var resources = {
+    txPlayer: new ex.Texture("/assets/player.png")
+};
+for (var r in resources) {
+    loader.addResource(resources[r]);
+}
 function keyHeld(key) {
     if (game.input.keyboard.isHeld(ex.Input.Keys[key])) {
         return true;
@@ -50,52 +56,122 @@ function keyReleased(key) {
 game.start(loader).then(function () {
     game.setAntialiasing(false);
     game.backgroundColor = palette[3];
-    var playerSprite = new ex.Sprite(txPlayer, 0, 0, 16, 16);
-    playerSprite.scale = new ex.Vector(grid.scale, grid.scale);
     var Player = (function (_super) {
         __extends(Player, _super);
         function Player() {
             _super.call(this);
             this.speed = 32;
+            this.facing = "up";
+            this.facingStack = [];
             this.mV = new ex.Vector(0, 0);
         }
+        Player.prototype.onInitialize = function (engine) {
+            var idleSpeed = 500;
+            var runSpeed = 200;
+            var spriteSheet = new ex.SpriteSheet(resources.txPlayer, 4, 5, 16, 16);
+            var idleAnimU = spriteSheet.getAnimationBetween(engine, 8, 10, idleSpeed);
+            idleAnimU.loop = true;
+            var idleAnimR = spriteSheet.getAnimationBetween(engine, 2, 4, idleSpeed);
+            idleAnimR.loop = true;
+            var idleAnimD = spriteSheet.getAnimationBetween(engine, 0, 2, idleSpeed);
+            idleAnimD.loop = true;
+            var idleAnimL = spriteSheet.getAnimationBetween(engine, 10, 12, idleSpeed);
+            idleAnimL.loop = true;
+            var walkAnimU = spriteSheet.getAnimationByIndices(engine, [12, 8, 13, 8], runSpeed);
+            walkAnimU.loop = true;
+            var walkAnimR = spriteSheet.getAnimationByIndices(engine, [6, 2, 7, 2], runSpeed);
+            walkAnimR.loop = true;
+            var walkAnimD = spriteSheet.getAnimationByIndices(engine, [4, 0, 5, 0], runSpeed);
+            walkAnimD.loop = true;
+            var walkAnimL = spriteSheet.getAnimationByIndices(engine, [14, 10, 15, 10], runSpeed);
+            walkAnimL.loop = true;
+            this.addDrawing("idleU", idleAnimU);
+            this.addDrawing("idleR", idleAnimR);
+            this.addDrawing("idleD", idleAnimD);
+            this.addDrawing("idleL", idleAnimL);
+            this.addDrawing("walkU", walkAnimU);
+            this.addDrawing("walkR", walkAnimR);
+            this.addDrawing("walkD", walkAnimD);
+            this.addDrawing("walkL", walkAnimL);
+            this.scale = new ex.Vector(grid.scale, grid.scale);
+        };
         Player.prototype.update = function (engine, delta) {
             _super.prototype.update.call(this, engine, delta);
             if (keyPressed("Up")) {
                 this.mV.addEqual(new ex.Vector(0, -1));
+                this.facingStack.push("up");
             }
             if (keyPressed("Right")) {
                 this.mV.addEqual(new ex.Vector(1, 0));
+                this.facingStack.push("right");
             }
             if (keyPressed("Down")) {
                 this.mV.addEqual(new ex.Vector(0, 1));
+                this.facingStack.push("down");
             }
             if (keyPressed("Left")) {
                 this.mV.addEqual(new ex.Vector(-1, 0));
+                this.facingStack.push("left");
             }
             if (keyReleased("Up")) {
                 this.mV.subEqual(new ex.Vector(0, -1));
+                this.facingStack.splice(this.facingStack.indexOf("up"), 1);
             }
             if (keyReleased("Right")) {
                 this.mV.subEqual(new ex.Vector(1, 0));
+                this.facingStack.splice(this.facingStack.indexOf("right"), 1);
             }
             if (keyReleased("Down")) {
                 this.mV.subEqual(new ex.Vector(0, 1));
+                this.facingStack.splice(this.facingStack.indexOf("down"), 1);
             }
             if (keyReleased("Left")) {
                 this.mV.subEqual(new ex.Vector(-1, 0));
+                this.facingStack.splice(this.facingStack.indexOf("left"), 1);
+            }
+            if (this.facingStack[this.facingStack.length - 1] != null) {
+                this.facing = this.facingStack[this.facingStack.length - 1];
             }
             if (this.mV.x != 0 || this.mV.y != 0) {
                 this.vel = this.mV.normalize().scale(100);
+                switch (this.facing) {
+                    case "up":
+                        this.setDrawing("walkU");
+                        break;
+                    case "right":
+                        this.setDrawing("walkR");
+                        break;
+                    case "down":
+                        this.setDrawing("walkD");
+                        break;
+                    case "left":
+                        this.setDrawing("walkL");
+                        break;
+                }
             }
             else {
                 this.vel.setTo(0, 0);
+                switch (this.facing) {
+                    case "up":
+                        this.setDrawing("idleU");
+                        break;
+                    case "right":
+                        this.setDrawing("idleR");
+                        break;
+                    case "down":
+                        this.setDrawing("idleD");
+                        break;
+                    case "left":
+                        this.setDrawing("idleL");
+                        break;
+                }
             }
         };
         return Player;
     }(ex.Actor));
     var player = new Player();
-    player.addDrawing(playerSprite);
+    player.x = game.getWidth() / 2;
+    player.y = game.getHeight() / 2;
     game.add(player);
 });
 //# sourceMappingURL=game.js.map
--- src/game.ts
+++ src/game.ts
@@ -18,8 +18,16 @@ let game = new ex.Engine({
   height: grid.size * grid.height * grid.scale
 });
 
-let txPlayer = new ex.Texture("/assets/player.png");
-let loader = new ex.Loader([txPlayer]);
+
+
+// Create the loader and load textures
+let loader = new ex.Loader();
+let resources = {
+  txPlayer: new ex.Texture("/assets/player.png")
+}
+for (let r in resources) { loader.addResource(resources[r]); }
+
+
 
 function keyHeld(key: string): boolean {
   if (game.input.keyboard.isHeld(ex.Input.Keys[key])) {
@@ -51,39 +59,109 @@ game.start(loader).then(function() {
 
   game.backgroundColor = palette[3];
 
-  let playerSprite = new ex.Sprite(txPlayer, 0, 0, 16, 16);
-  playerSprite.scale = new ex.Vector(grid.scale, grid.scale);
-
   class Player extends ex.Actor {
     private speed = 32;
+    private facing = "up";
+    private facingStack: string[] = [];
     private mV = new ex.Vector(0, 0);
 
-    constructor() {
-      super();
+    constructor() { 
+      super(); 
+    }
+
+    public onInitialize(engine: ex.Engine) {
+      let idleSpeed = 500;
+      let runSpeed = 200;
+
+      let spriteSheet = new ex.SpriteSheet(resources.txPlayer, 4, 5, 16, 16);
+
+      let idleAnimU = spriteSheet.getAnimationBetween(engine, 8, 10, idleSpeed);idleAnimU.loop = true;
+      let idleAnimR = spriteSheet.getAnimationBetween(engine, 2, 4, idleSpeed);idleAnimR.loop = true;
+      let idleAnimD = spriteSheet.getAnimationBetween(engine, 0, 2, idleSpeed);idleAnimD.loop = true;
+      let idleAnimL = spriteSheet.getAnimationBetween(engine, 10, 12, idleSpeed);idleAnimL.loop = true;
+
+      let walkAnimU = spriteSheet.getAnimationByIndices(engine, [12, 8, 13, 8], runSpeed);walkAnimU.loop = true;
+      let walkAnimR = spriteSheet.getAnimationByIndices(engine, [6, 2, 7, 2], runSpeed);walkAnimR.loop = true;
+      let walkAnimD = spriteSheet.getAnimationByIndices(engine, [4, 0, 5, 0], runSpeed);walkAnimD.loop = true;
+      let walkAnimL = spriteSheet.getAnimationByIndices(engine, [14, 10, 15, 10], runSpeed);walkAnimL.loop = true;
+
+      this.addDrawing("idleU", idleAnimU);
+      this.addDrawing("idleR", idleAnimR);
+      this.addDrawing("idleD", idleAnimD);
+      this.addDrawing("idleL", idleAnimL);
+
+      this.addDrawing("walkU", walkAnimU);
+      this.addDrawing("walkR", walkAnimR);
+      this.addDrawing("walkD", walkAnimD);
+      this.addDrawing("walkL", walkAnimL);
+
+      this.scale = new ex.Vector(grid.scale, grid.scale);
     }
 
     public update(engine: ex.Engine, delta) {
       super.update(engine, delta);
       
-      if (keyPressed("Up")) { this.mV.addEqual(new ex.Vector(0, -1)); }
-      if (keyPressed("Right")) { this.mV.addEqual(new ex.Vector(1, 0)); }
-      if (keyPressed("Down")) { this.mV.addEqual(new ex.Vector(0, 1)); }
-      if (keyPressed("Left")) { this.mV.addEqual(new ex.Vector(-1, 0)); }
+      if (keyPressed("Up")) { 
+        this.mV.addEqual(new ex.Vector(0, -1));
+        this.facingStack.push("up");
+      }
+      if (keyPressed("Right")) { 
+        this.mV.addEqual(new ex.Vector(1, 0));
+        this.facingStack.push("right");
+      }
+      if (keyPressed("Down")) { 
+        this.mV.addEqual(new ex.Vector(0, 1));
+        this.facingStack.push("down");
+      }
+      if (keyPressed("Left")) { 
+        this.mV.addEqual(new ex.Vector(-1, 0));
+        this.facingStack.push("left");
+      }
       
-      if (keyReleased("Up")) { this.mV.subEqual(new ex.Vector(0, -1)); }
-      if (keyReleased("Right")) { this.mV.subEqual(new ex.Vector(1, 0)); }
-      if (keyReleased("Down")) { this.mV.subEqual(new ex.Vector(0, 1)); }
-      if (keyReleased("Left")) { this.mV.subEqual(new ex.Vector(-1, 0)); }
+      if (keyReleased("Up")) {
+        this.mV.subEqual(new ex.Vector(0, -1));
+        this.facingStack.splice(this.facingStack.indexOf("up"), 1);
+      }
+      if (keyReleased("Right")) {
+        this.mV.subEqual(new ex.Vector(1, 0));
+        this.facingStack.splice(this.facingStack.indexOf("right"), 1);
+      }
+      if (keyReleased("Down")) {
+        this.mV.subEqual(new ex.Vector(0, 1));
+        this.facingStack.splice(this.facingStack.indexOf("down"), 1);
+      }
+      if (keyReleased("Left")) {
+        this.mV.subEqual(new ex.Vector(-1, 0));
+        this.facingStack.splice(this.facingStack.indexOf("left"), 1);
+      }
+
+      if (this.facingStack[this.facingStack.length-1] != null) {
+        this.facing = this.facingStack[this.facingStack.length-1];
+      }
 
       if (this.mV.x != 0 || this.mV.y != 0) {
         this.vel = this.mV.normalize().scale(100);
+        switch (this.facing) {
+          case "up": this.setDrawing("walkU"); break;
+          case "right": this.setDrawing("walkR"); break;
+          case "down": this.setDrawing("walkD"); break;
+          case "left": this.setDrawing("walkL"); break;
+        }
+
       } else {
         this.vel.setTo(0, 0);
+        switch (this.facing) {
+          case "up": this.setDrawing("idleU"); break;
+          case "right": this.setDrawing("idleR"); break;
+          case "down": this.setDrawing("idleD"); break;
+          case "left": this.setDrawing("idleL"); break;
+        }
       }
     }
   }
 
   let player = new Player();
-  player.addDrawing(playerSprite);
+  player.x = game.getWidth() / 2;
+  player.y = game.getHeight() / 2;
   game.add(player);
 }); 
