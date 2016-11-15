000b1fc6965361f14cc83e30fd7375957c021cea
Reece Bennett <reece.bennett1@gmail.com>
2016-11-15 20:27:41 +0000

    Start making player class

--- dist/game.js
+++ dist/game.js
@@ -1,9 +1,14 @@
+var __extends = (this && this.__extends) || function (d, b) {
+    for (var p in b) if (b.hasOwnProperty(p)) d[p] = b[p];
+    function __() { this.constructor = d; }
+    d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
+};
 // World constants
 var grid = {
     size: 8,
     width: 20,
     height: 18,
-    scale: 5
+    scale: 4
 };
 var palette = [
     ex.Color.fromHex("#cecead"),
@@ -15,7 +20,82 @@ var game = new ex.Engine({
     width: grid.size * grid.width * grid.scale,
     height: grid.size * grid.height * grid.scale
 });
-game.backgroundColor = palette[3];
-// Start the engine
-game.start();
+var txPlayer = new ex.Texture("/assets/player.png");
+var loader = new ex.Loader([txPlayer]);
+function keyHeld(key) {
+    if (game.input.keyboard.isHeld(ex.Input.Keys[key])) {
+        return true;
+    }
+    else {
+        return false;
+    }
+}
+function keyPressed(key) {
+    if (game.input.keyboard.wasPressed(ex.Input.Keys[key])) {
+        return true;
+    }
+    else {
+        return false;
+    }
+}
+function keyReleased(key) {
+    if (game.input.keyboard.wasReleased(ex.Input.Keys[key])) {
+        return true;
+    }
+    else {
+        return false;
+    }
+}
+// Start game
+game.start(loader).then(function () {
+    game.setAntialiasing(false);
+    game.backgroundColor = palette[3];
+    var playerSprite = new ex.Sprite(txPlayer, 0, 0, 16, 16);
+    playerSprite.scale = new ex.Vector(grid.scale, grid.scale);
+    var Player = (function (_super) {
+        __extends(Player, _super);
+        function Player() {
+            _super.call(this);
+            this.speed = 32;
+            this.mV = new ex.Vector(0, 0);
+        }
+        Player.prototype.update = function (engine, delta) {
+            _super.prototype.update.call(this, engine, delta);
+            if (keyPressed("Up")) {
+                this.mV.addEqual(new ex.Vector(0, -1));
+            }
+            if (keyPressed("Right")) {
+                this.mV.addEqual(new ex.Vector(1, 0));
+            }
+            if (keyPressed("Down")) {
+                this.mV.addEqual(new ex.Vector(0, 1));
+            }
+            if (keyPressed("Left")) {
+                this.mV.addEqual(new ex.Vector(-1, 0));
+            }
+            if (keyReleased("Up")) {
+                this.mV.subEqual(new ex.Vector(0, -1));
+            }
+            if (keyReleased("Right")) {
+                this.mV.subEqual(new ex.Vector(1, 0));
+            }
+            if (keyReleased("Down")) {
+                this.mV.subEqual(new ex.Vector(0, 1));
+            }
+            if (keyReleased("Left")) {
+                this.mV.subEqual(new ex.Vector(-1, 0));
+            }
+            if (this.mV.x != 0 || this.mV.y != 0) {
+                this.vel = this.mV.normalize().scale(100);
+            }
+            else {
+                this.vel.setTo(0, 0);
+            }
+        };
+        return Player;
+    }(ex.Actor));
+    var player = new Player();
+    player.addDrawing(playerSprite);
+    game.add(player);
+});
 //# sourceMappingURL=game.js.map
--- src/game.ts
+++ src/game.ts
@@ -3,7 +3,7 @@ let grid = {
   size: 8,
   width: 20,
   height: 18,
-  scale: 5
+  scale: 4
 } 
 
 let palette = [
@@ -18,9 +18,72 @@ let game = new ex.Engine({
   height: grid.size * grid.height * grid.scale
 });
 
-game.backgroundColor = palette[3];
+let txPlayer = new ex.Texture("/assets/player.png");
+let loader = new ex.Loader([txPlayer]);
 
+function keyHeld(key: string): boolean {
+  if (game.input.keyboard.isHeld(ex.Input.Keys[key])) {
+    return true;
+  } else {
+    return false;
+  }
+}
 
+function keyPressed(key: string): boolean {
+  if (game.input.keyboard.wasPressed(ex.Input.Keys[key])) {
+    return true;
+  } else {
+    return false;
+  }
+}
 
-// Start the engine
-game.start();
+function keyReleased(key: string): boolean {
+  if (game.input.keyboard.wasReleased(ex.Input.Keys[key])) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+// Start game
+game.start(loader).then(function() {
+  game.setAntialiasing(false);
+
+  game.backgroundColor = palette[3];
+
+  let playerSprite = new ex.Sprite(txPlayer, 0, 0, 16, 16);
+  playerSprite.scale = new ex.Vector(grid.scale, grid.scale);
+
+  class Player extends ex.Actor {
+    private speed = 32;
+    private mV = new ex.Vector(0, 0);
+
+    constructor() {
+      super();
+    }
+
+    public update(engine: ex.Engine, delta) {
+      super.update(engine, delta);
+      
+      if (keyPressed("Up")) { this.mV.addEqual(new ex.Vector(0, -1)); }
+      if (keyPressed("Right")) { this.mV.addEqual(new ex.Vector(1, 0)); }
+      if (keyPressed("Down")) { this.mV.addEqual(new ex.Vector(0, 1)); }
+      if (keyPressed("Left")) { this.mV.addEqual(new ex.Vector(-1, 0)); }
+      
+      if (keyReleased("Up")) { this.mV.subEqual(new ex.Vector(0, -1)); }
+      if (keyReleased("Right")) { this.mV.subEqual(new ex.Vector(1, 0)); }
+      if (keyReleased("Down")) { this.mV.subEqual(new ex.Vector(0, 1)); }
+      if (keyReleased("Left")) { this.mV.subEqual(new ex.Vector(-1, 0)); }
+
+      if (this.mV.x != 0 || this.mV.y != 0) {
+        this.vel = this.mV.normalize().scale(100);
+      } else {
+        this.vel.setTo(0, 0);
+      }
+    }
+  }
+
+  let player = new Player();
+  player.addDrawing(playerSprite);
+  game.add(player);
+}); 
